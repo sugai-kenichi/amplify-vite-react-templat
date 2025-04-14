40eac494c4467eb5140dd9708217f52f3c35cce6
sugai <kinspe0401@gmail.com>
2025-04-14 17:25:57 +0900

    add auth hideSignUp

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator hideSignUp>
       <App />
     </Authenticator>
   </React.StrictMode>
